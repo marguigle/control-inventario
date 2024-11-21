33c2aeb3e634555625faec61f32f793ed6612a7b
Marcelo G Iglesia <marguigle@yahoo.com.ar>
2024-11-21 18:29:02 -0300

    se crea funcion de mostrar todos los productos

--- app.py
+++ app.py
@@ -17,21 +17,7 @@ def main():
             print("-" * 80)
             menu_principal()
         elif opcion_elegida == "2":
-            if lista_productos:
-
-                print(f"{'ID':^35} {'Nombre':^15} {'Precio':^10} {'Cantidad':^10}")
-                print("-" * 80)
-                for producto in lista_productos:
-                    print(
-                        f"{producto['id']:^5} {producto['nombre']:^15} {producto['precio']:^10.2f} {producto['cantidad']:^10}"
-                    )
-                print("-" * 80)
-                menu_principal()
-            else:
-                print("-" * 80)
-                print("No hay productos en la lista.")
-                print("-" * 80)
-                menu_principal()
+            mostrar_lista_completa()
         elif opcion_elegida == "3":
             eliminar_producto()
             print("-" * 80)
--- funciones.py
+++ funciones.py
@@ -34,6 +34,26 @@ def agregar_producto():
 
     lista_productos.append(nuevo_producto.__dict__)
 
+    # mostrar todos los productos
+
+
+def mostrar_lista_completa():
+    if lista_productos:
+
+        print(f"{'ID':^35} {'Nombre':^15} {'Precio':^10} {'Cantidad':^10}")
+        print("-" * 80)
+        for producto in lista_productos:
+            print(
+                f"{producto['id']:^5} {producto['nombre']:^15} {producto['precio']:^10.2f} {producto['cantidad']:^10}"
+            )
+        print("-" * 80)
+        menu_principal()
+    else:
+        print("-" * 80)
+        print("No hay productos en la lista.")
+        print("-" * 80)
+        menu_principal()
+
 
 #### buscar producto por nombre
 def buscar_un_producto():
